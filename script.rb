96df0a6023a8bcee700a5a1caee1331a4989ce78
Monatric <nawpnawp8@gmail.com>
2024-06-12 03:10:58 +0800

    Add the basic foundations for the project

---
+++ script.rb
@@ -0,0 +1,22 @@
+DICTIONARY = [
+    "below",
+    "down",
+    "go",
+    "going",
+    "horn",
+    "how",
+    "howdy",
+    "it",
+    "i",
+    "low",
+    "own",
+    "part",
+    "partner",
+    "sit"
+]
+
+h = Hash.new
+
+def substrings(words, dictionary)
+
+end
