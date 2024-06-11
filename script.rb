3fb2178fea59e208cb9bc7ecc2518cdee07e6070
Monatric <nawpnawp8@gmail.com>
2024-06-12 03:58:07 +0800

    Add a key value pair if the word matches in the dictionary

--- script.rb
+++ script.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 DICTIONARY = [
     "below",
     "down",
@@ -15,8 +17,22 @@ DICTIONARY = [
     "sit"
 ]
 
-h = Hash.new
+H = Hash.new
 
 def substrings(words, dictionary)
-
+    word_array = words.split(' ')
+    word_array.map do |word_array_element|
+        dictionary.each do |filtered_words_element|
+            if word_array_element.match?(filtered_words_element) == true
+                H[filtered_words_element] = 1
+                # if H.include?(filtered_words_element)
+                #     H.each { |key, val| H[filtered_words_element] += 1}
+                # end
+            end
+        end
+            
+        p H
+    end
+    p H
 end
+p substrings("belowlow", DICTIONARY)
