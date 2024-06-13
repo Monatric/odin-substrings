ed55586e76aebd37d953510804e98f795bda531d
Monatric <nawpnawp8@gmail.com>
2024-06-13 18:11:03 +0800

    Add +1 per words that match

--- script.rb
+++ script.rb
@@ -24,15 +24,15 @@ def substrings(words, dictionary)
     word_array.map do |word_array_element|
         dictionary.each do |filtered_words_element|
             if word_array_element.match?(filtered_words_element) == true
-                H[filtered_words_element] = 1
-                # if H.include?(filtered_words_element)
-                #     H.each { |key, val| H[filtered_words_element] += 1}
-                # end
+                if H.include?(filtered_words_element)
+                    H[filtered_words_element] += 1
+                else
+                    H[filtered_words_element] = 1
+                    
+                end
             end
         end
-            
-        p H
     end
-    p H
+    H
 end
-p substrings("belowlow", DICTIONARY)
+p substrings("Howdy partner, sit down! How's it going?", DICTIONARY)
